12250d02b608f9814144cdc060ff6b6952e33d3b
PRADEEP CHANDRA VARMA MANDAPATI <mpch.varma@gmail.com>
2026-01-29 13:16:22 +0000

    final version

--- mcp_app/server.py
+++ mcp_app/server.py
@@ -1,4 +1,4 @@
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
 from mcp_app.tools import recommend_movies
 
 mcp = FastMCP("MoviesRAG")
@@ -6,6 +6,7 @@ mcp.tool()(recommend_movies)
 
 if __name__ == "__main__":
     mcp.run(
-        transport="streamable-http",
+        transport="streamable-http",host="0.0.0.0",
+        port=8000,
         
     )
